8121224c81157ea18f3661c79dfcc6d70b800a4c
Serikbol <serikbol.talgatuly@nu.edu.kz>
2023-07-07 17:53:07 +0500

    DB and upload

--- base/views.py
+++ base/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponseRedirect
+from .form import ImageForm
+from .models import Image
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(request.path_info)   # Redirect to a success page after saving the image
+    else:
+        form = ImageForm()
+    return render(request, 'index.html', {'form': form})
