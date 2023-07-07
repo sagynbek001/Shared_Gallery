fb22104dd060f420d59e7f3a72a0a4efe35e37bb
Serikbol <serikbol.talgatuly@nu.edu.kz>
2023-07-08 00:08:39 +0500

    SG-8: Better Style

--- base/urls.py
+++ base/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="image")
+    path("", views.index, name="image"),
+    path('gallery/', views.gallery_view, name="gallery"),
 ]
--- base/views.py
+++ base/views.py
@@ -4,12 +4,17 @@ from .form import ImageForm
 from .models import Image
 
 # Create your views here.
+def gallery_view(request):
+    return render(request, "gallery.html")
+
+    
 def index(request):
     if request.method == 'POST':
         form = ImageForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect(request.path_info)   # Redirect to a success page after saving the image
+            return HttpResponseRedirect(request.path_info)   
     else:
         form = ImageForm()
     return render(request, 'index.html', {'form': form})
+
